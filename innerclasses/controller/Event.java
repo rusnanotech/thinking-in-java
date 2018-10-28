cfd75b6d12d799c96a408270c2e387868cdeb881
Vasily Markov <vasily@markov.biz>
2018-10-28 14:50:55 +0300

    Added modified expample of greenhouse control system from chapter 'Inner Classes'

---
+++ innerclasses/controller/Event.java
@@ -0,0 +1,24 @@
+/****************************************************
+ * Modified version of the original example
+ * from TIJ4 by replacing System.nanoTime()
+ * to System.currentTimeMillis() increasing delay time.
+ ****************************************************/
+//: innerclasses/controller/Event.java
+// The common methods for any control event.
+package biz.markov.thinking.innerclasses.controller;
+
+public abstract class Event {
+  private long eventTime;
+  protected final long delayTime;
+  public Event(long delayTime) {
+    this.delayTime = delayTime;
+    start();
+  }
+  public void start() { // Allows restarting
+    eventTime = System.currentTimeMillis() + delayTime;
+  }
+  public boolean ready() {
+    return System.currentTimeMillis() >= eventTime;
+  }
+  public abstract void action();
+} ///:~
---
+++ innerclasses/controller/GreenhouseController.java
@@ -0,0 +1,57 @@
+/****************************************************
+ * Uses modified version of the original class Event
+ * from TIJ4 by increasing delay time for greater clarity.
+ ****************************************************/
+//: innerclasses/GreenhouseController.java
+// Configure and execute the greenhouse system.
+// {Args: 5000}
+package biz.markov.thinking.innerclasses.controller;
+
+public class GreenhouseController {
+  public static void main(String[] args) {
+    GreenhouseControls gc = new GreenhouseControls();
+    // Instead of hard-wiring, you could parse
+    // configuration information from a text file here:
+    gc.addEvent(gc.new Bell(900));
+    Event[] eventList = {
+      gc.new ThermostatNight(0),
+      gc.new LightOn(200),
+      gc.new LightOff(400),
+      gc.new WaterOn(600),
+      gc.new WaterOff(800),
+      gc.new ThermostatDay(1400)
+    };
+    gc.addEvent(gc.new Restart(2000, eventList));
+    if(args.length == 1)
+      gc.addEvent(
+        new GreenhouseControls.Terminate(
+          new Integer(args[0])));
+    gc.run();
+  }
+} /* Output:
+Thermostat on night setting
+Light is on
+Light is off
+Greenhouse water is on
+Greenhouse water is off
+Bing!
+Thermostat on day setting
+Bing!
+Restarting system
+Thermostat on night setting
+Light is on
+Light is off
+Greenhouse water is on
+Bing!
+Greenhouse water is off
+Thermostat on day setting
+Bing!
+Restarting system
+Thermostat on night setting
+Light is on
+Light is off
+Bing!
+Greenhouse water is on
+Greenhouse water is off
+Terminating
+*///:~
