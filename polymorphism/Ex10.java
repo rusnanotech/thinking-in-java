fd621dbd46460c1d79524d1e58c8061ca4c7f8a3
Vasily Markov <vasily@markov.biz>
2018-11-10 21:18:01 +0300

    Correction in chapter 'Polymorphism'

--- polymorphism/Ex10.java
+++ polymorphism/Ex10.java
@@ -8,8 +8,26 @@
  ***********************************************/
 package biz.markov.thinking.polymorphism;
 
+class Ex10_base {
+    void m1() {
+        System.out.println("Ex10_Base.m1()");
+        m2();
+    }
+    
+    void m2() {
+        System.out.println("Ex10_Base.m2()");
+    }
+}
+
+class Ex10_Derived extends Ex10_base {
+    void m2() {
+        System.out.println("Ex10_Derived.m2()");
+    }
+}
+
 public class Ex10 {
-    public static void main(String[] args) {
-        biz.markov.thinking.polymorphism.shape.Ex10.main(args);
+    public static void main(String args[]) {
+        Ex10_base base = new Ex10_Derived();
+        base.m1();
     }
 }
