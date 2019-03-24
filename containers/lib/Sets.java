dbd3af6ec9db9c50b1dbdbd5310d7d0009e511b4
Vasily Markov <vasily@markov.biz>
2019-03-24 17:18:37 +0300

    Refactored library for Containers in Depth chapter

--- containers/lib/Sets.java
+++ containers/lib/Sets.java
@@ -1,10 +1,15 @@
 package biz.markov.thinking.containers.lib;
 
 import java.util.Iterator;
-import java.util.SortedSet;
+import java.util.Set;
 
-public class SortedSets {
-    public static boolean isEquals(SortedSet<?> set1, SortedSet<?> set2) {
+public class Sets {
+
+    /**
+     * Checks two Set implementations for equality.
+     * Returns true if the two sets are equal and have the same order.
+     * */
+    public static boolean isEquals(Set<?> set1, Set<?> set2) {
         Iterator<?> itr1 = set1.iterator();
         Iterator<?> itr2 = set2.iterator();
 
