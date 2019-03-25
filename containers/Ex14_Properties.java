08ac5993bb5c445e159ff9d64a9a77ae5f34f377
Vasily Markov <vasily@markov.biz>
2019-03-25 23:41:00 +0300

    Completed ex.14 of Containers in Depth Chapter

---
+++ containers/Ex14_Properties.java
@@ -0,0 +1,17 @@
+/****************** Exercise 14 *****************
+ * Show that java.util.Properties works in the above
+ * program (containers/Maps.java from the book).
+ ***********************************************/
+package biz.markov.thinking.containers;
+
+import biz.markov.thinking.containers.lib.mindview.Maps;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class Ex14_Properties {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        Maps.test((Map<Integer, String>) (Object) new Properties());
+    }
+}
