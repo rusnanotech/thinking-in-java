6b85b2fc1c3cc6e60359514b7cad2eb7e0143c86
Vasily Markov <VMarkov@bellintegrator.com>
2020-06-15 10:49:33 +0300

    ex. 26 of Concurrency chapter

---
+++ concurrency/Ex26_Restaurant.java
@@ -0,0 +1,107 @@
+package biz.markov.thinking.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static net.mindview.util.Print.print;
+import static net.mindview.util.Print.printnb;
+
+class Meal {
+    private final int orderNum;
+
+    public Meal(int orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    public String toString() {
+        return "Meal " + orderNum;
+    }
+}
+
+class WaitPerson implements Runnable {
+    private final Ex26_Restaurant restaurant;
+
+    public WaitPerson(Ex26_Restaurant r) {
+        restaurant = r;
+    }
+
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    while (restaurant.meal == null)
+                        wait(); // ... for the chef to produce a meal
+                }
+                print("Waitperson got " + restaurant.meal);
+                synchronized (restaurant.chef) {
+                    restaurant.meal = null;
+                    restaurant.chef.notifyAll(); // Ready for another
+                }
+            }
+        } catch (InterruptedException e) {
+            print("WaitPerson interrupted");
+        }
+    }
+}
+
+class Chef implements Runnable {
+    private final Ex26_Restaurant restaurant;
+    private int count = 0;
+
+    public Chef(Ex26_Restaurant r) {
+        restaurant = r;
+    }
+
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    while (restaurant.meal != null)
+                        wait(); // ... for the meal to be taken
+                }
+                if (++count == 10) {
+                    print("Out of food, closing");
+                    restaurant.exec.shutdownNow();
+                }
+                printnb("Order up! ");
+                synchronized (restaurant.waitPerson) {
+                    restaurant.meal = new Meal(count);
+                    restaurant.waitPerson.notifyAll();
+                }
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            print("Chef interrupted");
+        }
+    }
+}
+
+public class Ex26_Restaurant {
+    Meal meal;
+    final ExecutorService exec = Executors.newCachedThreadPool();
+    final WaitPerson waitPerson = new WaitPerson(this);
+    final Chef chef = new Chef(this);
+
+    public Ex26_Restaurant() {
+        exec.execute(chef);
+        exec.execute(waitPerson);
+    }
+
+    public static void main(String[] args) {
+        new Ex26_Restaurant();
+    }
+} /* Output:
+Order up! Waitperson got Meal 1
+Order up! Waitperson got Meal 2
+Order up! Waitperson got Meal 3
+Order up! Waitperson got Meal 4
+Order up! Waitperson got Meal 5
+Order up! Waitperson got Meal 6
+Order up! Waitperson got Meal 7
+Order up! Waitperson got Meal 8
+Order up! Waitperson got Meal 9
+Out of food, closing
+WaitPerson interrupted
+Order up! Chef interrupted
+*///:~
